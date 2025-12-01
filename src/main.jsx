298ab5e3b5d587636ba2aee554cf47827b42595e
Marcel Masełko <marcel@MacBook-Air-Marcel.local>
2025-12-01 16:27:55 +0100

    Naprawa bledu: instalacja react-helmet-async i dodanie providera

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async';
+// 1. DODAJ TEN IMPORT:
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* 2. OTOCZ WSZYSTKO HELMET PROVIDEREM: */}
     <HelmetProvider>
       <BrowserRouter>
         <App />
-        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
